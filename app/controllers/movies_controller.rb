1ab1b29d2b44ba59d075eca9680cfef417a44ca7
tcabaj <tomcab95@gmail.com>
2017-03-19 19:01:38 +0100

    Project

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,52 @@
+class MoviesController < ApplicationController
+before_action :find_movie, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @movies = Movie.all.order("created_at DESC")
+  end
+
+  def show
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @movie.update(movie_params)
+      redirect_to movie_path(@movie)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @movie.destroy
+    redirect_to root_path
+  end
+
+
+  private
+    def movie_params
+      params.require(:movie).permit(:title, :description, :director)
+    end
+
+    def find_movie
+      @movie = Movie.find(params[:id])
+    end
+
+end
